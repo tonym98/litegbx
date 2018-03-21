34049321789f5efce49de8ef216bc7f2a35b72a8
tonym98 <tonymorris1998@gmail.com>
2018-03-21 23:07:38 +0000

    Update netbase.cpp

--- src/netbase.cpp
+++ src/netbase.cpp
@@ -4,7 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifdef HAVE_CONFIG_H
-#include "config/vivo-config.h"
+#include "config/gobyte-config.h"
 #endif
 
 #include "netbase.h"
