4d716e4f397ad08b62c6782dca07827d96e42f45
tonym98 <tonymorris1998@gmail.com>
2018-03-21 23:12:23 +0000

    Update rpcblockchain.cpp

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-2015 The Bitcoin Core developers
-// Copyright (c) 2014-2017 The Vivo Core developers
+// Copyright (c) 2014-2017 The Dash Core developers
+// Copyright (c) 2017-2018 The GoByte Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -610,8 +611,8 @@ UniValue gettxout(const UniValue& params, bool fHelp)
             "     \"hex\" : \"hex\",        (string) \n"
             "     \"reqSigs\" : n,          (numeric) Number of required signatures\n"
             "     \"type\" : \"pubkeyhash\", (string) The type, eg pubkeyhash\n"
-            "     \"addresses\" : [          (array of string) array of vivo addresses\n"
-            "        \"vivoaddress\"     (string) vivo address\n"
+            "     \"addresses\" : [          (array of string) array of gobyte addresses\n"
+            "        \"gobyteaddress\"     (string) gobyte address\n"
             "        ,...\n"
             "     ]\n"
             "  },\n"
