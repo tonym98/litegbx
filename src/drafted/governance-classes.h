6182cbb52c6a65fb2b59425d2b2add33df1cdea7
tonym98 <tonymorris1998@gmail.com>
2018-03-21 23:35:47 +0000

    Update governance-classes.h

--- src/drafted/governance-classes.h
+++ src/drafted/governance-classes.h
@@ -1,4 +1,5 @@
-// Copyright (c) 2014-2017 The Vivo Core developers
+// Copyright (c) 2014-2017 The Dash Core developers
+// Copyright (c) 2017-2018 The GoByte Core developers
 
 /*
  * FIELDS AND CLASSIFICATION
@@ -45,7 +46,7 @@
  * =========================
  *
  *   // network
- *   CVivoNetwork lvl, network-type, network-status, network-error, milestone-status*
+ *   CGoByteNetwork lvl, network-type, network-status, network-error, milestone-status*
  *   CCategory lvl, category-type, status, status-error
  *   CNetworkGlobalVariable lvl, global-type, status, status-error
  *   // base: actor
@@ -79,7 +80,7 @@
  *  TREE STRUCTURE
  *  ===========================================
  * 
- *  VIVO NETWORK (ROOT)
+ *  GOBYTE NETWORK (ROOT)
  *      -> NETWORK GLOBOLS
  *          -> SWITCHES, SETTINGS
  *      -> CATEGORIES
@@ -117,7 +118,7 @@ private:
     int nLevel;
     std::string strCategory;
 
-    // Current OBJECT STATUS (see http://govman.vivo.org/index.php/Documentation_:_Status_Field)
+    // Current OBJECT STATUS (see http://govman.gobyte.org/index.php/Documentation_:_Status_Field)
     int nStatusID;
     std::string strStatusMessage;
 
@@ -130,7 +131,7 @@ public:
 };
 
 // // root node
-class CVivoNetwork : public CGovernanceObject
+class CGoByteNetwork : public CGovernanceObject
 {
 private:
     std::string strName;
@@ -138,7 +139,7 @@ private:
 
 
 public:
-    CVivoNetwork(UniValue objIn)
+    CGoByteNetwork(UniValue objIn)
     {
         strName = objIn["name"].get_str();
         strURL = objIn["name"].get_str();
@@ -174,9 +175,9 @@ public:
 
 };
 
-// // can be under: VivoNetwork
+// // can be under: GoByteNetwork
 // //   -- signature requirements : Key1(User)
-// class CVivoNetworkVariable : public CGovernanceObject
+// class CGoByteNetworkVariable : public CGovernanceObject
 // {
 // private:
 
@@ -279,7 +280,7 @@ public:
 //     // isRootCategory()
 //     // {
 //     //     // root categories won't have categories as parents
-//     //     return (IsType() == VivoNetwork);
+//     //     return (IsType() == GoByteNetwork);
 //     // }
 
 //     // isSubcategoryOf(std::string strParentName)
