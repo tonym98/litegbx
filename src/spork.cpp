24397194d083c83806ec13031c9d8fc2d46622e3
tonym98 <tonymorris1998@gmail.com>
2018-03-21 23:19:29 +0000

    Update spork.cpp

--- src/spork.cpp
+++ src/spork.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2014-2017 The Vivo Core developers
+// Copyright (c) 2014-2017 The Dash Core developers
+// Copyright (c) 2017-2018 The GoByte Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -17,7 +18,7 @@ std::map<uint256, CSporkMessage> mapSporks;
 
 void CSporkManager::ProcessSpork(CNode* pfrom, std::string& strCommand, CDataStream& vRecv)
 {
-    if(fLiteMode) return; // disable all Vivo specific functionality
+    if(fLiteMode) return; // disable all GoByte specific functionality
 
     if (strCommand == NetMsgType::SPORK) {
 
